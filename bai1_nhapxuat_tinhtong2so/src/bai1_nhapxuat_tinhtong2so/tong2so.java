d90e3fdc294226e3be0557f9b235b05dd241c654
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-02-05 20:05:18 +0700

    thêm nhập thông tin từ bàn phím

--- bai1_nhapxuat_tinhtong2so/src/bai1_nhapxuat_tinhtong2so/tong2so.java
+++ bai1_nhapxuat_tinhtong2so/src/bai1_nhapxuat_tinhtong2so/tong2so.java
@@ -1,9 +1,20 @@
 package bai1_nhapxuat_tinhtong2so;
-
+import java.util.Scanner;
 public class tong2so {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//khai báo
+		double num1, num 2, sum;
+		//khai bao doi tuong Scanner
+		Scanner banPhim = new Scanner(System.in);
+		
+		//In ra thông tin, mời người dùng nhập số thứ nhất
+		System.out.printf("Moi nhap so thu nhat: ");
+		num1= banPhim.nextDouble();
+		//In ra thong tin, mời người dùng nhập số thứ hai
+		System.out.printf("Moi nhap so thu hai: ");
+		num2= banPhim.nextDouble();
+
 
 	}
 
