f44f8d59392409d3134bd9adfa1af9704a2eaaca
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-02-05 20:21:22 +0700

    hoàn thành bài

--- bai1_nhapxuat_tinhtong2so/src/bai1_nhapxuat_tinhtong2so/tinhtong2so.java
+++ bai1_nhapxuat_tinhtong2so/src/bai1_nhapxuat_tinhtong2so/tinhtong2so.java
@@ -1,10 +1,11 @@
 package bai1_nhapxuat_tinhtong2so;
+
 import java.util.Scanner;
-public class tong2so {
+public class tinhtong2so {
 
 	public static void main(String[] args) {
 		//khai báo
-		double num1, num 2, sum;
+		double num1, num2, sum;
 		//khai bao doi tuong Scanner
 		Scanner banPhim = new Scanner(System.in);
 		
@@ -15,7 +16,19 @@ public class tong2so {
 		System.out.printf("Moi nhap so thu hai: ");
 		num2= banPhim.nextDouble();
 
-
+        //tinh toan
+		sum = num1 + num2;
+		// xuat ket qua ra man hinh
+	//	System.out.print("Tổng của " + num1 + "và" + num2 + "là" +sum);
+	//	System.out.print("Tổng của ");
+	//	System.out.print(num1);
+	//	System.out.print(" và ");
+	//	System.out.print(num2);
+	//	System.out.print(" là ");
+	//	System.out.print(sum);
+		
+		System.out.printf("Tổng của %.2f và %.2f là %.2f", num1, num2, sum);
+		
 	}
 
 }
