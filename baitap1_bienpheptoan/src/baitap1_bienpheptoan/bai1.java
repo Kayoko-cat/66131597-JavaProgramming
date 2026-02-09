6795a5f6d5130fec6e88532849ffd60e3780d00c
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-02-09 23:32:56 +0700

    them khai bao va tinh toan

--- baitap1_bienpheptoan/src/baitap1_bienpheptoan/bai1.java
+++ baitap1_bienpheptoan/src/baitap1_bienpheptoan/bai1.java
@@ -3,7 +3,12 @@ package baitap1_bienpheptoan;
 public class bai1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// khai bao cac bien
+		int a = 15;
+		int b = 4;
+		double c =3.5;
+		//tinh toan
+		double thuong = a/b;
 
 	}
 
