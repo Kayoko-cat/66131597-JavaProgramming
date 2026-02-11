dd52f7cf93ae1b71126f81e38ed0c31b9c227be2
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-02-11 19:15:34 +0700

    tao thuoc tinh

--- btsinhvien/src/btsinhvien/doituong.java
+++ btsinhvien/src/btsinhvien/doituong.java
@@ -1,5 +1,12 @@
 package btsinhvien;
 
 public class doituong {
-
+//thuoctinh
+	String ten;
+	int tuoi;
+	String maSV;
+	//phuong thuc 
+	void hocBai(String monHoc) {
+		System.out.println( ten + " dang hoc " + monHoc);
+	}
 }
