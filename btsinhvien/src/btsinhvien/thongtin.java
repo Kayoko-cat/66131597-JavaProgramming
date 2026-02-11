43f5120c195793bc623bc6e50728005e3b7ac479
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-02-11 19:24:10 +0700

    fix loi va hoan thien bai

--- btsinhvien/src/btsinhvien/thongtin.java
+++ btsinhvien/src/btsinhvien/thongtin.java
@@ -1,18 +1,22 @@
 package btsinhvien;
 
-public class thongtin {
+public class thongtin	 {
 	  public static void main(String[] args) {
 		// Tạo đối tượng sinhVien1 từ lớp SinhVien
-	        SinhVien sinhVien1 = new SinhVien();
+		  Sinhvien sinhVien1 = new Sinhvien();
 	        sinhVien1.ten = "Nguyễn Văn A";
 	        sinhVien1.tuoi = 20;
 	        sinhVien1.maSV = "SV001";
 		  
 		  
 	     // Tạo đối tượng sinhVien2
-	        SinhVien sinhVien2 = new SinhVien();
-	        sinhVien2.ten = "Trần Thị B";
+	        Sinhvien sinhVien2 = new Sinhvien();
+	       	sinhVien2.ten = "Trần Thị B";
 	        sinhVien2.tuoi = 21;
 	        sinhVien2.maSV = "SV002";
+	   
+	      //  // Gọi phương thức
+	        sinhVien1.hocBai("Toán");
+	        sinhVien2.hocBai("Văn");
 	  }
 }
