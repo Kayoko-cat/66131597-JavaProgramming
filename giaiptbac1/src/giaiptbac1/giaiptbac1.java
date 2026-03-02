86a10df8df57a820ae35ed1a9c534a99e277fcef
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-02 22:54:26 +0700

    nhap dieu kien tinh toan

--- giaiptbac1/src/giaiptbac1/giaiptbac1.java
+++ giaiptbac1/src/giaiptbac1/giaiptbac1.java
@@ -10,6 +10,16 @@ public class giaiptbac1 {
 
         System.out.print("Nhap b: ");
         double b = sc.nextDouble();
+        
+        if (a == 0) {
+            if (b == 0) {
+                System.out.println("Phuong trinh vo so nghiem");
+            } else {
+                System.out.println("Phuong trinh vo nghiem");
+            }
+        } else {
+            double x = -b / a;
+            System.out.println("Nghiem x = " + x);
 
 	}
 
