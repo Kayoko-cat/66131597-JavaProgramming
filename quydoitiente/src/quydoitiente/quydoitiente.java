60526926de1ff1a797ad08dd865330231022ffb8
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-02 22:34:53 +0700

    nhao tien

--- quydoitiente/src/quydoitiente/quydoitiente.java
+++ quydoitiente/src/quydoitiente/quydoitiente.java
@@ -1,10 +1,17 @@
 package quydoitiente;
-
+import java.util.Scanner;
 public class quydoitiente {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	Scanner sc = new Scanner(System.in);
+		
+	System.out.print("tien usd : ");
+	double tienusd= sc.nextDouble();
+	
+	System.out.print("tien eur : ");
+	double tieneur= sc.nextDouble();
 
+	
 	}
 
 }
