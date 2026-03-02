f888d92c5b663bb8ac51b2afd5439e5e86fad8dd
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-02 22:05:42 +0700

    nhập vào số giờ làm và lương theo giờ

--- tinh tien luong/src/tinhtienluong.java
+++ tinh tien luong/src/tinhtienluong.java
@@ -1,9 +1,15 @@
+package tinhtienluong
+import java.util.Scanner;
 
 public class tinhtienluong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner sc = new Scanner(System.in);
+				
+		System.out.print("nhập số giờ làm ");
+        double sogio = sc.nextDouble();
+		System.out.print("nhập lương theo giờ ");
+		double luongio =sc.nextDouble();
 	}
 
 }
