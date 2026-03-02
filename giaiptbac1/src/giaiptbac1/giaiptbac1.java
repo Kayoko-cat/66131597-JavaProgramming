33c06b3ea47e9e19b8f0415e28b75972091e8bb9
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-02 22:50:18 +0700

    nhap a b

--- giaiptbac1/src/giaiptbac1/giaiptbac1.java
+++ giaiptbac1/src/giaiptbac1/giaiptbac1.java
@@ -1,9 +1,15 @@
 package giaiptbac1;
-
+import java.util.Scanner;
 public class giaiptbac1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Nhap a: ");
+        double a = sc.nextDouble();
+
+        System.out.print("Nhap b: ");
+        double b = sc.nextDouble();
 
 	}
 
