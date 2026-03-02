c753a56939a72598f27925f99bc8e03f088b90c5
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-02 22:21:09 +0700

    nhập đk tính toán

--- tinhtienluong/src/tinhtienluong/tinhtienluong.java
+++ tinhtienluong/src/tinhtienluong/tinhtienluong.java
@@ -1,19 +1,25 @@
 package tinhtienluong;
 import java.util.Scanner;
-
 public class tinhtienluong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		  Scanner sc = new Scanner(System.in);
+        System.out.print("Nhập số giờ làm: ");
+        double soGio = sc.nextDouble();
 
-	        System.out.print("Nhập số giờ làm: ");
-	        double soGio = sc.nextDouble();
+        System.out.print("Nhập lương theo giờ: ");
+        double luongGio = sc.nextDouble();
 
-	        System.out.print("Nhập lương theo giờ: ");
-	        double luongGio = sc.nextDouble();
+        double tongLuong;
+        if (soGio <= 40) {
+            tongLuong = soGio * luongGio;
+        } else {
+            double gioVuot = soGio - 40;
+            tongLuong = 40 * luongGio + gioVuot * luongGio * 1.5;
+        }
 
-	        double tongLuong;
+        System.out.println("Tổng lương: " + tongLuong);
 
-	}
-
+        sc.close();
+}
 }
