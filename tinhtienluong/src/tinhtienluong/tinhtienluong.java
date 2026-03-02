f5e6d1247ef80364dab35fd9997047ab5b1aaf1e
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-02 22:16:59 +0700

    nhap gia tri va khai bao

--- tinhtienluong/src/tinhtienluong/tinhtienluong.java
+++ tinhtienluong/src/tinhtienluong/tinhtienluong.java
@@ -1,9 +1,18 @@
 package tinhtienluong;
+import java.util.Scanner;
 
 public class tinhtienluong {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		  Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Nhập số giờ làm: ");
+	        double soGio = sc.nextDouble();
+
+	        System.out.print("Nhập lương theo giờ: ");
+	        double luongGio = sc.nextDouble();
+
+	        double tongLuong;
 
 	}
 
