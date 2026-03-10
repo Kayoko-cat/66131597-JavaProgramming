3fc5c672f85da3e909f96784dcf838adcd70e517
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-10 20:32:15 +0700

    fix

---
+++ vidu OOP Hamtao Getter/src/ChuNhat.java
@@ -0,0 +1,55 @@
+
+
+public class ChuNhat {
+
+    // khai bao cac thuoc tinh / du lieu cua lop / truong thong tin
+    private double dai;
+    private double rong;
+    //
+
+    // ham tao (constructor)
+    public ChuNhat() {
+        dai = 10;
+        rong = 5;
+    }
+
+    public ChuNhat(double dai, double rong) {
+        this.dai = dai;
+        this.rong = rong;
+    }
+
+    // public ChuNhat(double daiM, double rongM) {
+    //     dai = daiM;
+    //     rong = rongM;
+    // }
+
+    // cac getter
+    double getDai() {
+        return dai;
+    }
+
+    double getRong() {
+        return rong;
+    }
+
+    // --- cac setter
+    //void setDai(double daiM)
+    //{
+    //    dai = daiM;
+    //}
+
+    //void setRong(double rongM)
+    //{
+    //    rong = rongM;
+    //}
+
+    void setDai(double dai)
+    {
+        this.dai = dai;
+    }
+
+    void setRong(double rong)
+    {
+        this.rong = rong;
+    }
+}
--- vidu OOP Hamtao Getter/src/run.java
+++ vidu OOP Hamtao Getter/src/run.java
@@ -1,4 +1,10 @@
 
 public class run {
 
+	public static void main(String[] args) {
+		// tao moi doi tuong
+		ChuNhat cn1 = new chuNhat();
+		// tao doi tuong cn1 dai =10 rong= 5
+		ChuNhat cn2 = new ChuNhat(20,15);
+	}
 }
