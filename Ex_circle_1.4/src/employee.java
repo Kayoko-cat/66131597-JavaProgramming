a666c0ee4d910117e27f343a4a19c578f594b757
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-11 21:52:54 +0700

    khai bao

---
+++ Ex_circle_1.4/src/employee.java
@@ -0,0 +1,12 @@
+
+public class employee {
+	private int id, salary;
+	private String firstName, lastName;
+	public Employee(int id, String firstName, String lastName, int salary)
+	{
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.salary = salary;
+	}
+}
