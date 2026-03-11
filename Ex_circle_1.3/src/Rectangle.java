6b5dcc60a55a5c5be1064954e71e8b9c00d8bfbd
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-11 20:41:04 +0700

    getter setter

--- Ex_circle_1.3/src/Rectangle.java
+++ Ex_circle_1.3/src/Rectangle.java
@@ -13,4 +13,18 @@ public class Rectangle {
 	    this.length = length;
 	    this.width  = width;
 	}
+	// getter setter
+	public float getLength() {
+	    return length;
+	}
+	public void setLength(float length) {
+	    this.length = length;
+	}
+
+	public float getWidth() {
+	    return width;
+	}
+	public void setWidth(float width) {
+	    this.width = width;
+	}
 }
