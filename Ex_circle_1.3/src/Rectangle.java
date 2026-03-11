ce13c010f5d97e1bc8caf6a4da5d1e0222607b94
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-11 20:34:38 +0700

    constructor

---
+++ Ex_circle_1.3/src/Rectangle.java
@@ -0,0 +1,16 @@
+
+public class Rectangle {
+
+	public class Retamgle {
+	    private float length = 1.0f;  // chiều dài, mặc định = 1.0
+	    private float width  = 1.0f;  // chiều rộng, mặc định = 1.0
+	}
+	
+	public Rectangle() { }
+
+	// Hàm tạo có tham số
+	public Rectangle(float length, float width) {
+	    this.length = length;
+	    this.width  = width;
+	}
+}
