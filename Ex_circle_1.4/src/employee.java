4788857aa1da7a73720bc7c20aaa1f00fe528aec
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-11 21:54:31 +0700

    gettter setter

--- Ex_circle_1.4/src/employee.java
+++ Ex_circle_1.4/src/employee.java
@@ -1,5 +1,5 @@
 
-public class employee {
+public class Employee {
 	private int id, salary;
 	private String firstName, lastName;
 	public Employee(int id, String firstName, String lastName, int salary)
@@ -9,4 +9,20 @@ public class employee {
 		this.lastName = lastName;
 		this.salary = salary;
 	}
-}
+	public int getId()
+	{
+		return this.id;
+	}
+	public int getSalary()
+	{
+		return this.salary;
+	}
+	public String getFirstName()
+	{
+		return this.firstName;
+	}
+	public String getLastName()
+	{
+		return this.lastName;
+	}
+	
