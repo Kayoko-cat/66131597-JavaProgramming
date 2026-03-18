af4df8f59dd434743c481ae0b6f0218191325c61
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-18 19:25:56 +0700

    tao lop sinh vien

--- B11_vidu2/src/ntu/khoata/lopsinhvien.java
+++ B11_vidu2/src/ntu/khoata/lopsinhvien.java
@@ -1,5 +1,27 @@
 package ntu.khoata;
+import java.util.ArrayList;
+import java.util.List;
 
-public class lopsinhvien {
-
+// Lớp SinhVien
+class SinhVien {
+    private String maSV;
+    private String hoTen;
+    private double diemTB;
+    
+    public SinhVien(String maSV, String hoTen, double diemTB) {
+        this.maSV = maSV;
+        this.hoTen = hoTen;
+        this.diemTB = diemTB;
+    }
+    
+    // Getter
+    public String getMaSV() { return maSV; }
+    public String getHoTen() { return hoTen; }
+    public double getDiemTB() { return diemTB; }
+    
+    
+    public String toString() {
+    	String res = "Ban: " + hoTen + " co mssv la " + maSV + " co diem trung binh la " + diemTB;
+        return res;
+    }
 }
