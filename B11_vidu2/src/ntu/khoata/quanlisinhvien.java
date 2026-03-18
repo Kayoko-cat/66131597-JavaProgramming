927aefea4d306be2dd33c7f09ed249fe24a536e5
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-18 19:34:42 +0700

    them sinh vien va tim sinh vien theo ma

---
+++ B11_vidu2/src/ntu/khoata/quanlisinhvien.java
@@ -0,0 +1,26 @@
+package ntu.khoata;
+import java.util.ArrayList;
+import java.util.List;
+public class quanlisinhvien {
+    private List<SinhVien> danhSachSV;
+    
+    public quanlisinhvien() {
+        danhSachSV = new ArrayList<>();
+    }
+    
+    // Thêm sinh viên - add()
+    public void themSinhVien(SinhVien sv) {
+        danhSachSV.add(sv);
+        System.out.println("Đã thêm: " + sv.getHoTen());
+    }
+    
+    // Tìm sinh viên theo mã - sử dụng get() và vòng lặp
+    public SinhVien timTheoMa(String maSV) {
+        for (SinhVien sv : danhSachSV) {
+            if (sv.getMaSV().equals(maSV)) {
+                return sv;
+            }
+        }
+        return null;
+    }
+}
