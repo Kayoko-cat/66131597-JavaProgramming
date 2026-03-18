9836c8171260313011c9bac0d01c39846ecdfd1f
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-18 18:28:42 +0700

    tao  chuc nang nhap du lieu nhap danh sach

--- VD_ArraylistSonguyen/src/ntu/khoa/runMain.java
+++ VD_ArraylistSonguyen/src/ntu/khoa/runMain.java
@@ -1,10 +1,28 @@
 package ntu.khoa;
 
+import java.util.ArrayList;
+
+import java.util.Scanner;
+
 public class runMain {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Khai bao bien danh sach de chua cac so nguyen
+		ArrayList<Integer> dsSoNguyen = new ArrayList<Integer>();
+		
+		//a. nhap 10 phan tu tu ban phim
+		Scanner banphim = new Scanner(System.in);
+      
+		for(int i =0 ;i<10 ;i++) {
+		//Nhap 1 phan tu
+		   // in ra mot huong dan cho nguoi dung 
+	//	System.out.println("Moi nhap phan tu thu  " + i + ": ");
+			System.out.print("a["+ i +"]=");
+		// cho nguoi dung nhap , cat vao bien ta
+		Integer tam = banphim.nextInt();
+				// them vao danh sach
+		dsSoNguyen.add(tam);
 	}
 
+}
 }
