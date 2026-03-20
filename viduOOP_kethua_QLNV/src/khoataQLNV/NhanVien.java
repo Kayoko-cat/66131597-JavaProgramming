b8af788801cd2fff1315fd46579989bd0b4d3c69
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-03-20 18:05:23 +0700

    hoan thanh lop nhan vien

---
+++ viduOOP_kethua_QLNV/src/khoataQLNV/NhanVien.java
@@ -0,0 +1,33 @@
+package khoataQLNV;
+
+public class NhanVien {
+  // thuoc tinh 
+ private String maNV;
+ private String HoTen;
+ private double LuongCoBan;
+ // ham tao
+ public NhanVien(String maNV, String hoTen, double luongCoBan) {
+	super();
+	this.maNV = maNV;
+	HoTen = hoTen;
+	LuongCoBan = luongCoBan;
+ }
+ public NhanVien() {
+	super();
+ }
+ 
+ public double tinhLuong() {
+	 return LuongCoBan;
+ }
+ @Override
+ public String toString() {
+	return "NhanVien [maNV=" + maNV + ", HoTen=" + HoTen + ", LuongCoBan=" + LuongCoBan + "]";
+ }
+ 
+  public void hienThiThongTin() {
+	  System.out.println("MA NV: " + maNV);
+	  System.out.println("Ho ten: " + HoTen);
+	  System.out.println("Luong Co ban: " + LuongCoBan);
+  }
+  
+}
