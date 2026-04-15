7522e8e41d81235d337d6569c9a9228385097d53
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-04-15 20:27:36 +0700

    hoanthanh

--- vidustreamDocDuChuoi/src/khoata/vidudocchuoi.java
+++ vidustreamDocDuChuoi/src/khoata/vidudocchuoi.java
@@ -1,6 +1,8 @@
 package khoata;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
@@ -8,22 +10,29 @@ public class vidudocchuoi {
   
 	public static void main(String[] args) throws IOException{
 		
+		
+		FileInputStream fInput = new FileInputStream("baitho.txt");
 		// khai bao bien doi tuong InputStreamReader
-		InputStreamReader ipReader = new InputStreamReader(System.in);
+		
+		InputStreamReader ipReader = new InputStreamReader(fInput);
 		// khai bao bien doi tuong buffee
 		
 		BufferedReader bufReader = new BufferedReader(ipReader);
 		
-		//in ra mot cau huong dan de nguoi dung biet can lam gi
-		System.out.print("Nhap 1 chuoi: ");
 		
-		// Doc du lieu tu ban phim , cat vao 1 bien chuoi
 		
-		String lineDocDuoc = bufReader.readLine();
+		
+		// Doc 1 dong du lieu tu file , cat vao 1 bien chuoi
+		
+		String line1 = bufReader.readLine();
 		
 		// in ra dong doc duoc 
-		System.out.print(lineDocDuoc);
+		System.out.println(line1);
 		
+        String line2 = bufReader.readLine();
+		
+		
+		System.out.print(line2);
 		}
 	
 }
