3f72ce85410f437915181a8e61642a50ef3e054b
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-04-15 20:19:48 +0700

    doc tu ban phim

--- vidustreamDocDuChuoi/src/khoata/vidudocchuoi.java
+++ vidustreamDocDuChuoi/src/khoata/vidudocchuoi.java
@@ -1,5 +1,29 @@
 package khoata;
 
-public class vidudocchuoi {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class vidudocchuoi {
+  
+	public static void main(String[] args) throws IOException{
+		
+		// khai bao bien doi tuong InputStreamReader
+		InputStreamReader ipReader = new InputStreamReader(System.in);
+		// khai bao bien doi tuong buffee
+		
+		BufferedReader bufReader = new BufferedReader(ipReader);
+		
+		//in ra mot cau huong dan de nguoi dung biet can lam gi
+		System.out.print("Nhap 1 chuoi: ");
+		
+		// Doc du lieu tu ban phim , cat vao 1 bien chuoi
+		
+		String lineDocDuoc = bufReader.readLine();
+		
+		// in ra dong doc duoc 
+		System.out.print(lineDocDuoc);
+		
+		}
+	
 }
