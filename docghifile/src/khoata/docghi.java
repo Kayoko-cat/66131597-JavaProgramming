2284aa7aa911e53530c15eff766cdcf8e7c61dbe
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-04-15 23:10:28 +0700

    test

---
+++ docghifile/src/khoata/docghi.java
@@ -0,0 +1,30 @@
+package khoata;
+import java.io.*;
+class KtoD
+{
+public static void main(String args[]) throws IOException
+{
+String str;
+FileWriter fw;
+BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+ try
+{
+	 fw = new FileWriter("test.txt");
+}
+catch(IOException exc)
+{
+System.out.println("Khong the mo file."); return ;
+}
+ System.out.println("Nhap ('stop' de ket thuc chuong trinh).");
+ do
+{
+System.out.print(": ");
+str = br.readLine();
+if(str.compareTo("stop") == 0) break;
+str = str + "\r\n";
+fw.write(str);
+} while(str.compareTo("stop") != 0);
+
+fw.close();
+}
+}
