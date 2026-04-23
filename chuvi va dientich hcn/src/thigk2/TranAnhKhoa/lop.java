cd47d08e830d9ea5dc4ec62d96f2dcd69cac3de9
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-04-23 07:40:49 +0700

    lần thứ 2

--- chuvi va dientich hcn/src/thigk2/TranAnhKhoa/lop.java
+++ chuvi va dientich hcn/src/thigk2/TranAnhKhoa/lop.java
@@ -1,5 +1,30 @@
 package thigk2.TranAnhKhoa;
 
 public class lop {
+ private double x1, y1, x2 , y2;
+//Constructor
+ public lop(double x1, double y1, double x2, double y2) {
+     this.x1 = x1;
+     this.y1 = y1;
+     this.x2 = x2;
+     this.y2 = y2;
+ }
+ // Phương thức tính chiều dài
+ private double getChieuDai() {
+     return Math.abs(x1 - x2);
+ }
 
+ // Phương thức tính chiều rộng
+ private double getChieuRong() {
+     return Math.abs(y1 - y2);
+ }
+//Phương thức tính chu vi
+ public double tinhChuVi() {
+     return 2 * (getChieuDai() + getChieuRong());
+ }
+
+ // Phương thức tính diện tích
+ public double tinhDienTich() {
+     return getChieuDai() * getChieuRong();
+ }
 }
