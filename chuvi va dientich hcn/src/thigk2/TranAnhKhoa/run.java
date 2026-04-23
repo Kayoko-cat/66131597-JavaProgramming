17251b83603e540d4313068449d59a7e4f3fd4c7
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-04-23 07:49:57 +0700

    lần 3

--- chuvi va dientich hcn/src/thigk2/TranAnhKhoa/run.java
+++ chuvi va dientich hcn/src/thigk2/TranAnhKhoa/run.java
@@ -1,10 +1,33 @@
 package thigk2.TranAnhKhoa;
 
-public class run {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		
+public class run {
+	/* * TÓM TẮT CÁCH GIẢI:
+     * 1. Sử dụng Scanner để nhận tọa độ 2 điểm: (x1, y1) và (x2, y2).
+     * 2. Tính độ dài 2 cạnh dựa trên trị tuyệt đối hiệu tọa độ:
+     * 3. Khởi tạo đối tượng HinhChuNhat với các tham số vừa nhập.
+     * 4. Gọi phương thức tính toán và in kết quả ra màn hình.
+     */
+	
+		public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.print("Nhập x1, y1 (trên-trái): ");
+	        double x1 = sc.nextDouble();
+	        double y1 = sc.nextDouble();
+
+	        System.out.print("Nhập x2, y2 (dưới-phải): ");
+	        double x2 = sc.nextDouble();
+	        double y2 = sc.nextDouble();
+
+	        lop hcn = new lop(x1, y1, x2, y2);
+
+	        System.out.println("Chu vi: " + hcn.tinhChuVi());
+	        System.out.println("Diện tích: " + hcn.tinhDienTich());
+	        
+	        sc.close();
+	    }
+	
 
 	}
-
-}
---
+++ quanlisanpham/src/thigk2/TranAnhKhoa/run.java
@@ -0,0 +1,9 @@
+package thigk2.TranAnhKhoa;
+
+public class run {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
