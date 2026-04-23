3ccaeb20ae9b3e38424f04090ad154077920ee6b
Trần Anh Khoa <khoa123ddd@gmail.com>
2026-04-23 07:59:14 +0700

    lần thứ 4

--- quanlisanpham/src/thigk2/TranAnhKhoa/run.java
+++ quanlisanpham/src/thigk2/TranAnhKhoa/run.java
@@ -1,7 +1,14 @@
 package thigk2.TranAnhKhoa;
-
+import java.util.Scanner;
 public class run {
-
+	/* * TÓM TẮT CÁCH GIẢI CÂU 2:
+     * 1. Xây dựng lớp SanPham với các thuộc tính: mã, tên, loại và giá bán.
+     * 2. Sử dụng ArrayList để quản lý danh sách sản phẩm.
+     * 3. Hard-code 3 sản phẩm ban đầu và dùng vòng lặp để in danh sách.
+     * 4. Dùng Scanner để nhập thêm sản phẩm mới từ người dùng.
+     * 5. Duyệt danh sách bằng vòng lặp for-each và dùng equals() 
+     * để lọc các sản phẩm thuộc loại "Thực phẩm chức năng".
+     */
 	public static void main(String[] args) {
 		
 	}
